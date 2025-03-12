93c5f89d20810ac7ed615c81aac391db75885c12
AyushiSanskar <ayushi@sanskartechnolab.com>
2025-03-12 18:09:22 +0530

    Added Doctypes in Airport Shop Management Module

---
+++ airplane_mode/airport_shop_management/report/shop_status_per_airport/shop_status_per_airport.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Ayushi Dhamecha and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
