b36392f42414453c1534ebd7178c5fdc81c68ba0
AyushiSanskar <ayushi@sanskartechnolab.com>
2025-03-18 10:27:57 +0530

    Added Shop Types

--- airplane_mode/airport_shop_management/doctype/shop/shop.js
+++ airplane_mode/airport_shop_management/doctype/shop/shop.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2025, Ayushi Dhamecha and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Shop", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Shop", {
+    onload: function(frm) {
+        frm.set_query('shop_type', function() {
+            return {
+                filters: {
+                    'enabled': 1
+                }
+            };
+        });
+    }
+});
